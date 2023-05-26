8e394590c26e2b766ff1ca44c18f48dc2c0bfe5c
worldspawn <worldspawn.web@gmail.com>
2023-05-26 15:35:53 +0300

    fixed errors from eslint check

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,13 +5,13 @@ import getDifference from '../src/getDifference.js';
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1')
-    .option('-f, --format <type>', 'output format')
-    .argument('<filepath1>', 'path to file one')
-    .argument('<filepath2>', 'path to file two')
-    .action((filepath1, filepath2) => {
-        console.log(getDifference(filepath1, filepath2));
-    });
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>', 'path to file one')
+  .argument('<filepath2>', 'path to file two')
+  .action((filepath1, filepath2) => {
+    console.log(getDifference(filepath1, filepath2));
+  });
 
 program.parse();
--- src/getDifference.js
+++ src/getDifference.js
@@ -1,49 +1,49 @@
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'node:path';
-import parser from '../src/parser.js';
+import parser from './parser.js';
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const getData = (filepath) => {
-    const data = fs.readFileSync(filepath, 'utf-8');
-    return data;
+  const data = fs.readFileSync(filepath, 'utf-8');
+  return data;
 };
 
 const buildTree = (data1, data2) => {
-    const keys = Object.keys(data1).concat(Object.keys(data2));
-    const uniqProperties = _.sortBy(_.uniq(keys));
-    const result = [];
-    uniqProperties.map((key) => {
-        // if the keys and values equal
-        if (_.has(data1, key) && _.has(data2, key)) {
-            if (data1[key] === data2[key]) {
-                result.push(`   ${key}: ${data1[key]}`);
-            }
-        }
-        // if the keys equal, but values are diff
-        if (_.has(data1, key) && _.has(data2, key)) {
-            if (data1[key] !== data2[key]) {
-                result.push(` - ${key}: ${data1[key]}`);
-                result.push(` + ${key}: ${data2[key]}`);
-            }
-        }
-        // if the key is missing
-        if (_.has(data1, key) && !(_.has(data2, key))) {
-            result.push(` - ${key}: ${data1[key]}`);
-        }
-        // if new key appeared
-        if (!(_.has(data1, key)) && _.has(data2, key)) {
-            result.push(` + ${key}: ${data2[key]}`);
-        }
-    });
-    return `{\n${result.join('\n')}\n}`;
+  const keys = Object.keys(data1).concat(Object.keys(data2));
+  const uniqProperties = _.sortBy(_.uniq(keys));
+  const result = [];
+  uniqProperties.map((key) => {
+    // if the keys and values equal
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] === data2[key]) {
+        result.push(`   ${key}: ${data1[key]}`);
+      }
+    }
+    // if the keys equal, but values are diff
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] !== data2[key]) {
+        result.push(` - ${key}: ${data1[key]}`);
+        result.push(` + ${key}: ${data2[key]}`);
+      }
+    }
+    // if the key is missing
+    if (_.has(data1, key) && !(_.has(data2, key))) {
+      result.push(` - ${key}: ${data1[key]}`);
+    }
+    // if new key appeared
+    if (!(_.has(data1, key)) && _.has(data2, key)) {
+      result.push(` + ${key}: ${data2[key]}`);
+    }
+  });
+  return `{\n${result.join('\n')}\n}`;
 };
 
 const getDifference = (file1, file2) => {
-    const data1 = parser(getData(getAbsolutePath(file1)));
-    const data2 = parser(getData(getAbsolutePath(file2)));
-    const diff = buildTree(data1, data2);
-    return diff;
+  const data1 = parser(getData(getAbsolutePath(file1)));
+  const data2 = parser(getData(getAbsolutePath(file2)));
+  const diff = buildTree(data1, data2);
+  return diff;
 };
 
 export default getDifference;
